7f249f09d4681d0b87eda21a418360118143e0b4
Qibin <qbye0405@gmail.com>
2023-03-01 16:26:56 -0500

    queues

---
+++ queues.js
@@ -0,0 +1,26 @@
+/**
+ * Challenge 1: Draw and explain in a diagram of how the queue data structure works.
+ * 
+Challenge 2: Create 2 methods that are:
+• enqueue (insert a element at the end of the queue)
+• dequeue (remove a element from the start of the queue)
+
+Challenge 3: Create 3 additional methods that are:
+• peek (retrieves the element at the start of the queue, WITHOUT removing it)
+• isEmpty (checks if the queue is empty – should return true or false)
+o hint - what property of the queue can you use to build this method J?
+• printQueue (prints the elements in the queue)
+ */
+
+class Node {
+    constructor(value){
+    }
+    }
+    class Queue {
+    constructor(){
+    }
+    enqueue(val){
+    }
+    dequeue(){
+    }
+    }
