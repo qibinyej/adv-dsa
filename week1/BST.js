fbea59f99fbcb4ba5d75222a74c76cf690c66523
Qibin <qbye0405@gmail.com>
2023-03-07 13:12:50 -0500

    bst

---
+++ week1/BST.js
@@ -0,0 +1,25 @@
+/**
+ * Challenge 1: Given the following illustrations, identify each tree as:
+- Tree
+- Binary Tree
+- Binary Search Tree
+- Not Applicable
+
+Challenge 2: Create 3 methods that are:
+• insert (inserts a node as a child of the given parent node)
+• find (retrieves a given node)
+• contains (returns a Boolean if a given node exists)
+
+Challenge 3: Create 1 additional method that is:
+• remove (removes a node and its children from the BST)
+*/
+
+class Node {
+  constructor(value) {}
+}
+class BinarySearchTree {
+  constructor() {}
+  insert(value) {}
+  find(value) {}
+  contains(value) {}
+}
