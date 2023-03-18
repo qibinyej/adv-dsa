9893f5ffe7266a80fc463fd2f8c25e46591a642b
Qibin <qbye0405@gmail.com>
2023-03-18 01:21:50 -0400

    week3 added

---
+++ challenge4.js
@@ -0,0 +1,5 @@
+/**
+ * Challenge 4: Given the following binary heap tree, create a function called isMaxHeap(heap) that accepts a
+heap as a argument, and it should return either true or false if it’s a valid max heap tree.
+Example below should return TRUE
+ */
